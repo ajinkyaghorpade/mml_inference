e94c244c403db98d4f098a5f550f97eabd497803
Ajinkya <ajinkya@smart.mit.edu>
2018-05-11 17:14:14 -0400

    Added multicore libraries.

--- parallel_test.R
+++ parallel_test.R
@@ -6,6 +6,7 @@ library(bayesm);
 library(data.table);
 library(pdist);
 library(foreach);
+library(doMC);
 
 # Load required functions.
 source('./simulate_data.R');
@@ -27,6 +28,8 @@ source('./perform_var_hier_inf.R');
 # Set the seed for random experiments
 set.seed(100);
 
+registerDoMC(cores=30);
+
 #  Number of agents
 H_exp = c(250, 1000, 5000);
 
@@ -40,28 +43,30 @@ K_exp = c(3, 10);
 T_exp = c(1, 5 ,15, 25);
 
 # Heterogeneity of the agent population
-zeta = seq(from = -2, to = 2, by = 4/(K-1));
+
 # Low heterogeneity
-Omega_exp = list(low = diag(0.25, nrow = K, ncol = K), high = diag(1, nrow = K, ncol = K));
+Omega_exp = list(low = 0.25, high = 1);
 
 # Perform the experiments here.
 
 # For each type of different agent population size,do
-foreach (H_idx = 1:length(H_exp)) %do% {
+foreach (H_idx = 1:length(H_exp)) %dopar% {
   # For different sizes of attributes spaces, do
-  foreach (K_idx  =  1:length(K_exp)) %do% {
+  foreach (K_idx  =  1:length(K_exp)) %dopar% {
     # For different number of choice events per user, do
-    foreach (T_idx  =  1:length(T_exp))  %do%{
+    foreach (T_idx  =  1:length(T_exp))  %dopar%{
       # For different sizes of choices available per choice event, do
-      foreach (J_idx  =  1:length(J_exp))  %do%{
+      foreach (J_idx  =  1:length(J_exp))  %dopar%{
         # For different levels of heterogeneity in the user choices per event
-        foreach (Omega_idx  =  1:length(Omega_exp)) %do% {
+        foreach (Omega_idx  =  1:length(Omega_exp)) %dopar% {
           # Generate artificial data
+          
           H <- H_exp[H_idx];
           K <- K_exp[K_idx];
           T <- T_exp[T_idx];
           J <- J_exp[J_idx];
-          Omega <- Omega_exp[[Omega_idx]];
+          zeta = seq(from = -2, to = 2, by = 4/(K-1));
+          Omega <- diag(Omega_exp[[Omega_idx]], nrow = K, ncol = K);
           data <- generate_data(H, K, J, zeta, Omega);
           
           # Prepare data format for mlogit
